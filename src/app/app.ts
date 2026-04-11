5ae96b291c6fb9c9e37363e32e8476842e2b04d3
JustCode7 <harrypottz85@gmail.com>
2026-04-11 16:48:28 +0200

    init: pwa project with github pages deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PwaInstallService } from './service/pwa-install.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('orbital-evolution');
+  private pwaInstallService = inject(PwaInstallService);
 }
